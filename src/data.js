cd642ad935026a3fc1b18451b18012310ccb5d9b
Hojun Kim <hojunkim2@gmail.com>
2022-11-04 15:16:18 -0700

    added project and fixed responsive ui of proj page

--- src/data.js
+++ src/data.js
@@ -1,7 +1,12 @@
 export const products = [
-    {
-        id: 1,
-        img: "https://i.imgur.com/CbI1x8I.jpg",
-        link: "https://sheepboy-7cea7.firebaseapp.com/",
-    },
+   {
+      id: 1,
+      img: "https://i.imgur.com/CbI1x8I.jpg",
+      link: "https://sheepboy-7cea7.firebaseapp.com/",
+   },
+   {
+      id: 2,
+      img: "https://i.imgur.com/t4SKjRG.jpg",
+      link: "https://hojkim.github.io/weeklyplanner/",
+   },
 ];
